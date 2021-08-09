5634da73569e9c79a1ca1dc0b0bd0abca843f5f3
DESKTOP-G4G6U71\DataRecordAD <davide.cotterle@ni.com>
2021-08-09 09:32:31 +0100

    separated clients form protos

--- Clients/Python/HelloW/greeter_client.py
+++ Clients/Python/HelloW/greeter_client.py
@@ -28,7 +28,7 @@ def run():
     # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='Mike'))
     print("Greeter client received: " + response.message)
 
 
